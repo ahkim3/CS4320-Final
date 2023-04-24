3cdbb688f0091458ca200fa8c75c6a8d34d7a688
Andrew Kim <andrewhkim3@gmail.com>
2023-04-24 12:44:08 -0500

    Create form skeleton

--- server/app.py
+++ server/app.py
@@ -1,13 +1,51 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+from pymongo import MongoClient
+
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
+client = MongoClient("mongodb://localhost:27017/")
+db = client["users"]
+
+
+@app.route("/")
 def hello_world():
-    return 'Hello, World!'
+    return "Hello, World!"
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5555)
+@app.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email")
+    password = request.json.get("password")
+    user = db.users.find_one({"email": email, "password": password}, {"password": 0})
+    return jsonify(user)
+
+
+@app.route("/new-account", methods=["POST"])
+def new_account():
+    email = request.json.get("email")
+    password = request.json.get("password")
+    firstName = request.json.get("firstName")
+    lastName = request.json.get("lastName")
+    cellPhone = request.json.get("cellPhone")
+    user = db.users.find_one({"email": email})
+    if user:
+        return jsonify(None)
+    else:
+        db.users.insert_one(
+            {
+                "email": email,
+                "password": password,
+                "firstName": firstName,
+                "lastName": lastName,
+                "cellPhone": cellPhone,
+            }
+        )
+        new_user = db.users.find_one({"email": email}, {"password": 0})
+        return jsonify(new_user)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5555)
