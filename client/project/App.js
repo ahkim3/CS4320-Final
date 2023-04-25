f8e666a24d99449fb7a71192675f8592d9a1e685
tylerharsell <tharsell20@gmail.com>
2023-04-24 23:05:00 -0500

    added postal code and hours

--- client/project/App.js
+++ client/project/App.js
@@ -1,16 +1,17 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
-
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
-  const [message, setMessage] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
-  const [cellPhone, setCellPhone] = useState('')
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
-  const [isNewAccount, setIsNewAccount] = useState(false)
+  const [message, setMessage] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [cellPhone, setCellPhone] = useState("");
+  const [hours, setHours] = useState("");
+  const [postal, setPostal] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isNewAccount, setIsNewAccount] = useState(false);
 
   async function fetchMessage() {
     const response = await fetch(`http://localhost:5555/`);
@@ -20,52 +21,66 @@ export default function App() {
 
   async function handleLogin() {
     const response = await fetch(`http://localhost:5555/login`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ email, password })
+      body: JSON.stringify({ email, password }),
     });
     const user = await response.json();
     if (user) {
-      setIsLoggedIn(true)
-      setEmail(user.email)
-      setFirstName(user.firstName)
-      setLastName(user.lastName)
-      setCellPhone(user.cellPhone)
+      setIsLoggedIn(true);
+      setEmail(user.email);
+      setFirstName(user.firstName);
+      setLastName(user.lastName);
+      setCellPhone(user.cellPhone);
+      setHours(user.Hours);
+      setPostal(user.Postal);
     } else {
-      alert('Invalid email or password')
+      alert("Invalid email or password");
     }
   }
 
   async function handleNewAccount() {
     const response = await fetch(`http://localhost:5555/new-account`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ email, password, firstName, lastName, cellPhone })
+      body: JSON.stringify({
+        email,
+        password,
+        firstName,
+        lastName,
+        cellPhone,
+        hours,
+        postal,
+      }),
     });
     const user = await response.json();
     if (user) {
-      setIsNewAccount(false)
-      setIsLoggedIn(true)
-      setEmail(user.email)
-      setFirstName(user.firstName)
-      setLastName(user.lastName)
-      setCellPhone(user.cellPhone)
+      setIsNewAccount(false);
+      setIsLoggedIn(true);
+      setEmail(user.email);
+      setFirstName(user.firstName);
+      setLastName(user.lastName);
+      setCellPhone(user.cellPhone);
+      setHours(user.hours);
+      setPostal(user.postal);
     } else {
-      alert('Could not create new account')
+      alert("Could not create new account");
     }
   }
 
   function handleLogout() {
-    setIsLoggedIn(false)
-    setEmail('')
-    setPassword('')
-    setFirstName('')
-    setLastName('')
-    setCellPhone('')
+    setIsLoggedIn(false);
+    setEmail("");
+    setPassword("");
+    setFirstName("");
+    setLastName("");
+    setCellPhone("");
+    setHours("");
+    setPostal("");
   }
 
   if (!isLoggedIn) {
@@ -74,13 +89,13 @@ export default function App() {
         <Text>Email:</Text>
         <TextInput
           style={styles.input}
-          onChangeText={text => setEmail(text)}
+          onChangeText={(text) => setEmail(text)}
           value={email}
         />
         <Text>Password:</Text>
         <TextInput
           style={styles.input}
-          onChangeText={text => setPassword(text)}
+          onChangeText={(text) => setPassword(text)}
           value={password}
           secureTextEntry={true}
         />
@@ -89,20 +104,32 @@ export default function App() {
             <Text>First Name:</Text>
             <TextInput
               style={styles.input}
-              onChangeText={text => setFirstName(text)}
+              onChangeText={(text) => setFirstName(text)}
               value={firstName}
             />
             <Text>Last Name:</Text>
             <TextInput
               style={styles.input}
-              onChangeText={text => setLastName(text)}
+              onChangeText={(text) => setLastName(text)}
               value={lastName}
             />
             <Text>Cell Phone:</Text>
             <TextInput
               style={styles.input}
-              onChangeText={text => setCellPhone(text)}
+              onChangeText={(text) => setCellPhone(text)}
               value={cellPhone}
+            />
+            <Text>Hours:</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={(text) => setHours(text)}
+              value={hours}
+            />
+            <Text>Postal Address:</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={(text) => setPostal(text)}
+              value={postal}
             />
             <Button title="Create New Account" onPress={handleNewAccount} />
             <Button title="Cancel" onPress={() => setIsNewAccount(false)} />
@@ -110,7 +137,10 @@ export default function App() {
         ) : (
           <>
             <Button title="Login" onPress={handleLogin} />
-            <Button title="Create New Account" onPress={() => setIsNewAccount(true)} />
+            <Button
+              title="Create New Account"
+              onPress={() => setIsNewAccount(true)}
+            />
           </>
         )}
       </View>
@@ -118,28 +148,40 @@ export default function App() {
   } else {
     return (
       <View style={styles.container}>
-      <Text>Welcome, {firstName}!</Text>
-      <Text>Email: {email}</Text>
-      <Text>First Name:</Text>
-      <TextInput
-      style={styles.input}
-      onChangeText={text => setFirstName(text)}
-      value={firstName}
-      />
-      <Text>Last Name:</Text>
-      <TextInput
-      style={styles.input}
-      onChangeText={text => setLastName(text)}
-      value={lastName}
-      />
-      <Text>Cell Phone:</Text>
-      <TextInput
-      style={styles.input}
-      onChangeText={text => setCellPhone(text)}
-      value={cellPhone}
-      />
-      <Button title="Save Changes" onPress={() => alert('Changes saved!')} />
-      <Button title="Logout" onPress={handleLogout} />
+        <Text>Welcome, {firstName}!</Text>
+        <Text>Email: {email}</Text>
+        <Text>First Name:</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => setFirstName(text)}
+          value={firstName}
+        />
+        <Text>Last Name:</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => setLastName(text)}
+          value={lastName}
+        />
+        <Text>Cell Phone:</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => setCellPhone(text)}
+          value={cellPhone}
+        />
+        <Text>Hours:</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => setHours(text)}
+          value={hours}
+        />
+        <Text>Postal Address:</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => setPostal(text)}
+          value={postal}
+        />
+        <Button title="Save Changes" onPress={() => alert("Changes saved!")} />
+        <Button title="Logout" onPress={handleLogout} />
       </View>
     );
   }
@@ -147,21 +189,21 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-  flex: 1,
-  backgroundColor: '#fff',
-  alignItems: 'center',
-  justifyContent: 'center',
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
-  fontSize: 48,
-  fontWeight: 'bold',
-  marginBottom: 16,
+    fontSize: 48,
+    fontWeight: "bold",
+    marginBottom: 16,
   },
   input: {
-  borderWidth: 1,
-  borderColor: '#ccc',
-  padding: 8,
-  margin: 10,
-  width: '80%',
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 8,
+    margin: 10,
+    width: "80%",
   },
 });
